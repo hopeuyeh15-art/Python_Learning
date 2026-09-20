7ce4091d5e07f2103eddad51ac54a20198bea411
Hope Uyeh <Hopeuyeh15@gmail.com>
2026-09-20 20:45:14 +0100

    Add Python files

---
+++ print.py
@@ -0,0 +1,51 @@
+#What is print?
+    # MEANING: print is a Python Built in Funtion That Is Use To Show The Result Of Our Code. WE MUST USE THE print FUNTIon IF WE WANT TO SEE THE OUTPUT OF OUR CODE .
+#Key Note1: About print Funtion Is That ALWAYS WRITE THE print FUNCTION IN SMALL LETTERs 
+#Key Note2: ALWAYS PUT IN EVERY STATEMENT OR CODE AFTER THE print IN A PARENTHESIS
+
+
+print("---------------")
+print('  Learn Python')
+print('----------------')
+
+# LEARNING PATH BY ADDING DOUBLEQUOTE(\"), SINGLE QUOTE(\'), NEWLINES(\n), TAB(\t)
+print("\thope\"python learning journey\"")
+print("\t\'My Name is Hope Uyeh Eno\'\n \tAm from Akwa_Ibom State\n \tI was born in Year 2005\n \ti want to be good in \'programming\'")
+print("path:c:hope\\learning\\python\\journey")
+print('message 1\n')
+print('message 2\n')
+print('message1 \"hope\"')
+
+# LEARNING PATH WITH BACKSLASH(\\)
+print('\tLearning path: Python basics\\ Data engineering\\ AI')
+
+# LEARNING PATH WITH SINGLE LINE, ADDING NEWLINES(\n) AN TAB(\t) 
+print("\tLEARNING PATH:\n \t-Python Basic \n\t-Data Engineering \n\t-AI")
+
+# Adding NewLines by spliting  With Tripple Quote
+print("""\tLEARNING PATH:
+ \t-Python Basic 
+\t-Data Engineering 
+\t-AI""")
+
+
+X=1
+print('My Name is Hope')
+print('Hope is Learning Python')
+print('Hope want to be a python expert')
+
+
+# updating name using variable. KEYNOTE (Always Seperate varaible from the code with comma, )
+name ='Hope'
+print('My Name is', name)
+print( name,'is Learning Python')
+print( name,'want to be a python expert')
+
+
+name = input("enter ur name:")
+country = 'nigeria'
+print(name, 'come from', country)   
+
+text= ("hi")
+number= 10
+print(text)
